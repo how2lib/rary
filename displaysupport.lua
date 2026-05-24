f53b0fcd2cd043149e63d40f435eb4a978eac1d1
how2lib <9pyxpqs4yb@privaterelay.appleid.com>
2026-05-24 02:54:13 -0400

    Update displaysupport.lua

--- displaysupport.lua
+++ displaysupport.lua
@@ -1,28 +1,17 @@
-local VirtualInputManager = game:GetService("VirtualInputManager")
---// CLICK FUNCTION
+task.wait(1)
+
 local function click(x, y)
 
-	VirtualInputManager:SendMouseButtonEvent(
-		x,
-		y,
-		0,
-		true,
-		game,
-		0
-	)
+	local VirtualInputManager = game:FindService("VirtualInputManager")
 
+	if not VirtualInputManager then
+		return
+	end
+
+	VirtualInputManager:SendMouseButtonEvent(x,y,0,true,game,0)
 	task.wait(0.05)
-
-	VirtualInputManager:SendMouseButtonEvent(
-		x,
-		y,
-		0,
-		false,
-		game,
-		0
-	)
+	VirtualInputManager:SendMouseButtonEvent(x,y,0,false,game,0)
 end
-
 
 --  (WANTED AUTOFARM BUY CLICKS FOR DIFFERENT SCREEN SIZES)
 -- click(100,100) // click(X,Y)
