336c68c697c970af74379ecca4ad03054fd0587e
how2lib <9pyxpqs4yb@privaterelay.appleid.com>
2026-05-24 02:52:54 -0400

    Update displaysupport2.lua

--- displaysupport2.lua
+++ displaysupport2.lua
@@ -1,26 +1,16 @@
-local VirtualInputManager = game:GetService("VirtualInputManager")
---// CLICK FUNCTION
+task.wait(1)
+
 local function click(x, y)
 
-	VirtualInputManager:SendMouseButtonEvent(
-		x,
-		y,
-		0,
-		true,
-		game,
-		0
-	)
+	local VirtualInputManager = game:FindService("VirtualInputManager")
 
+	if not VirtualInputManager then
+		return
+	end
+
+	VirtualInputManager:SendMouseButtonEvent(x,y,0,true,game,0)
 	task.wait(0.05)
-
-	VirtualInputManager:SendMouseButtonEvent(
-		x,
-		y,
-		0,
-		false,
-		game,
-		0
-	)
+	VirtualInputManager:SendMouseButtonEvent(x,y,0,false,game,0)
 end
 
 
