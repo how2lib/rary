9c6fd96212b16755357b4f7c4137f655e3f84b00
how2lib <9pyxpqs4yb@privaterelay.appleid.com>
2026-05-24 02:33:48 -0400

    Update displaysupport.lua

--- displaysupport.lua
+++ displaysupport.lua
@@ -1,3 +1,28 @@
+--// CLICK FUNCTION
+local function click(x, y)
+
+	VirtualInputManager:SendMouseButtonEvent(
+		x,
+		y,
+		0,
+		true,
+		game,
+		0
+	)
+
+	task.wait(0.05)
+
+	VirtualInputManager:SendMouseButtonEvent(
+		x,
+		y,
+		0,
+		false,
+		game,
+		0
+	)
+end
+
+
 --  (WANTED AUTOFARM BUY CLICKS FOR DIFFERENT SCREEN SIZES)
 -- click(100,100) // click(X,Y)
 click(1016,887)
